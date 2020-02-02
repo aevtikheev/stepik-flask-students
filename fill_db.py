287b2a4e384b878b74a98c8fde3de0a667d281fc
aevtikheev <eft000@gmail.com>
2020-02-02 20:54:06 +0300

    Add data generation script

---
+++ fill_db.py
@@ -0,0 +1,74 @@
+"""
+Script that generates fake data in the DB.
+Creates few random users, groups, and applicants(assigned to created groups).
+"""
+import random
+import logging
+
+import faker
+
+from stepik_p3.app import app
+from stepik_p3 import models
+
+USER_AMOUNT = 3
+APPLICANTS_AMOUNT = 10
+GROUPS_AMOUNT = 5
+
+logging.basicConfig(filename='db.log', filemode='w')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def _create_users(amount):
+    fake = faker.Faker()
+    for _ in range(amount):
+        user = models.User(email=fake.email(),
+                           name=fake.name(),
+                           password=fake.word())
+        models.db.session.add(user)
+        logger.info(f'New user {user.name}:{user.password}')
+    models.db.session.commit()
+    return [user.id for user in models.db.session.query(models.User).all()]
+
+
+def _create_groups(amount):
+    fake = faker.Faker()
+    for _ in range(amount):
+        group = models.Group(
+            title=fake.word(),
+            status=random.choice(
+                [status.value for status in models.GroupStatus]),
+            course=random.choice(
+                [course.value for course in models.CourseType]),
+            start_date=fake.date_this_year()
+        )
+        models.db.session.add(group)
+        logger.info(f'New group {group.title}')
+    models.db.session.commit()
+    return [group.id for group in models.db.session.query(models.Group).all()]
+
+
+def _create_applicants(amount, available_group_ids):
+    fake = faker.Faker()
+    for _ in range(amount):
+        applicant = models.Applicant(
+            email=fake.email(),
+            phone=fake.phone_number(),
+            name=fake.name(),
+            group_id=random.choice(available_group_ids),
+            status=random.choice(
+                [status.value for status in models.ApplicantStatus])
+        )
+        models.db.session.add(applicant)
+        logger.info(f'New applicant: {applicant.name}')
+    models.db.session.commit()
+    return [applicant.id for applicant in
+            models.db.session.query(models.Applicant).all()]
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        models.db.create_all()
+        _create_users(USER_AMOUNT)
+        group_ids = _create_groups(GROUPS_AMOUNT)
+        _create_applicants(APPLICANTS_AMOUNT, group_ids)
