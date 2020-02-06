522e53df2a7334eeba959063b749dae29268c8aa
aevtikheev <eft000@gmail.com>
2020-02-06 19:51:53 +0300

    Move mail server settings to the config

--- stepik_p3/config.py
+++ stepik_p3/config.py
@@ -7,5 +7,12 @@ db_path = current_path.parent / "sqlite.db"
 class Config:
     DEBUG = False
     SECRET_KEY = "2f807b548e7344cb9fd91b7c33719f4a"
+
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + str(db_path)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = 'localhost'
+    MAIL_USE_TLC = True
+    MAIL_PORT = 587
+    MAIL_USERNAME = 'user@example.com'
+    MAIL_PASSWORD = 'password'
